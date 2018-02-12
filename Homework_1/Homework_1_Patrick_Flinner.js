34da6f7663320dbfb77d3b6790da92239370056d
Patrick Flinner <ptflinner@yahoo.com>
2018-02-12 12:57:01 -0800

    Finished the assignment. Might clean up code next

--- Homework_1/Homework_1_Patrick_Flinner.js
+++ Homework_1/Homework_1_Patrick_Flinner.js
@@ -1,6 +1,13 @@
+//Patrick Flinner
+//304607711
+//Homework 1
+
 function upperCase(phrase){
     let upper='';
     for(i in phrase){
+        
+        /* Makes the letter uppercase before adding it to the new string 
+        or it will ignore it */
         if(typeOfLetter(phrase.charAt(i))===0){
             upper+=createUpper(phrase.charAt(i));
         }
@@ -14,6 +21,9 @@ function upperCase(phrase){
 function lowerCase(phrase){
     let lower='';
     for(i in phrase){
+
+        /* Makes the letter lowercase before adding it to the new string 
+        or it will ignore it */
         if(typeOfLetter(phrase.charAt(i))===1){
             lower+=createLower(phrase.charAt(i));
         }
@@ -28,13 +38,14 @@ function sentenceCase(phrase,nouns){
     let sentence='';
     let cap=true;
 
+    /*Capitalizes letters at the start of a sentence, aka after a period, and words that are nouns in a list */
     for(i in phrase){
         if(phrase.charCodeAt(i)===46){
             cap=true;
             sentence+=phrase.charAt(i);
         }
         else if(typeOfLetter(phrase.charAt(i))===0 && cap){
-            sentence+=createLower(phrase.charAt(i));
+            sentence+=createUpper(phrase.charAt(i));
             cap=false;
         }
         else{
@@ -44,9 +55,12 @@ function sentenceCase(phrase,nouns){
             }
         }
     }
+
+    sentence=replaceWord(sentence,nouns)
     return sentence;
 }
 
+/* Capitalizes the first letter in a word */
 function capitalizedCase(phrase){
     let capitalize='';
     let cap=true;
@@ -68,6 +82,7 @@ function capitalizedCase(phrase){
     return capitalize;
 }
 
+/*alternates the letter that is capitalized*/
 function alternatingCase(phrase){
     let alternate='';
     let flip=true;
@@ -86,6 +101,7 @@ function alternatingCase(phrase){
     return alternate;
 }
 
+/*Capitalizes every word that is important. List determines what is not important*/
 function titleCase(phrase, list){
     let capitalize='';
     let cap=true;
@@ -104,9 +120,14 @@ function titleCase(phrase, list){
             cap=false;
         }
     }
+
+    /*Replaces words considered unimportant and then makes sure the letters at the start of a sentence are correct*/
+    capitalize=replaceWord(capitalize,list);
+    capitalize=sentenceCase(capitalize);
     return capitalize;
 }
 
+/*Start of every word is lowercase and the rest uppercase*/
 function inverseCase(phrase){
     let capitalize='';
     let cap=true;
@@ -132,7 +153,9 @@ function inverseCase(phrase){
     return capitalize;
 }
 
+/*Creates a list of objects that are a letter and how many times it occurs*/
 function getCharacterFrequency(str){
+    //Object
     let frequency={
         character:'',
         counter:0
@@ -140,15 +163,18 @@ function getCharacterFrequency(str){
 
     let frequencyList=[];
 
-    for(var i=0;i<str.length;i++){
+    for(i in str){
+        //Pushes the first letter into the array
         if(frequencyList.length===0){
             frequency.character=str.charAt(i);
             frequency.counter=1;
             frequencyList.push(frequency)
         }
         else{
+            //Begins checking if new letters are found
+            //New letters are pushed in while duplicates are incremented
             let length=frequencyList.length;
-            for(var j=0;j<length;j++){
+            for(j=0;j<length;j++){
                 let lower=-32;
                 if(typeOfLetter(frequencyList[j].character)===0){
                     lower= 32;
@@ -182,9 +208,10 @@ function getCharacterFrequency(str){
 
 }
 
+//Prints the object used in getCharacterFrequency
 function printCharacterFrequency(list){
     
-     for(var i=0;i<list.length;i++){
+     for(i in list){
          if(list[i].counter>1){
              console.log('\''+list[i].character+'\' occurs ' +list[i].counter+' times');
          }
@@ -195,54 +222,47 @@ function printCharacterFrequency(list){
      }
 }
 
-function findWord(phrase,str,value){
-    for(var i=0;i<phrase.length;i++){
-        for(var j=0;j<str.length;j++){
-            if(typeOfLetter(phrase.charAt(i))===0){
-                if(phrase.charCodeAt(i)===str[j].charCodeAt(0) 
-                ||phrase.charCodeAt(i)===str[j].charCodeAt(0)+32){
-                    for(x=0;x<str[j].length;x++){
-                        if(phrase.charCodeAt(x)){
-
-                        }
-                    }
-                }
-            }
-            else if(typeOfLetter(phrase.charAt(i))===1){
-                if(phrase.charCodeAt(i)===str[j].charCodeAt(0) 
-                ||phrase.charCodeAt(i)===str[j].charCodeAt(0)+32){
-                    for(x=0;x<str[j].length;x++){
-                        
-                    }
-                }
-            }
-        }
+//Uses regular expressions to replace words in a sentence. Will only replace whole words. Ignores case sensitivity. 
+function replaceWord(phrase,list){
+    for(i in list){
+        let replace=list[i];
+        let search=lowerCase(list[i]);
+        
+        search=new RegExp('\\b'+search+'\\b','gi');
+        phrase=phrase.replace(search,replace);
     }
+    return phrase;
 }
 
+//Returns a value depending on if a letter is uppercase, lowercase, or not a letter
 function typeOfLetter(char){
+    //Lower
     if(char.charCodeAt(0)>=97 && char.charCodeAt(0)<=122){
         return 0;
     }
+    //Upper
     else if (char.charCodeAt(0)>=65 && char.charCodeAt(0)<=90){
         return 1;
     }
+
+    //Neither
     return 2;
 }
 
+//Returns a lowercase letter
 function createLower(char){
     let ch=String.fromCharCode(char.charCodeAt(0)+32)
     return ch;
 }
 
+//Returns an uppercase letter
 function createUpper(char){
     let ch=String.fromCharCode(char.charCodeAt(0)-32)
     return ch;
 }
 
-
 function runStringFunctions(){
-    let str = 'I watched the storm, so beautiful yet terrific. The face of the moon was in shadow.'
+    let str = 'i watched the storm, so beautiful yet terrific. the face of the moon was in shadow.'
     
     let unconditionallyCapitalized = ['I', 'Moon', 'Shadow']
     let lowercaseWords = ['the', 'of', 'in', 'an']
