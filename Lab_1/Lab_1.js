3dfd7cce595806d41f321edf0e63d9756682518c
Patrick Flinner <ptflinner@yahoo.com>
2018-02-19 15:11:19 -0800

    Setting up lab 1

---
+++ Lab_1/Lab_1.js
@@ -0,0 +1,22 @@
+const getTimes=(argument,callback)=>{
+
+}
+
+const orderTimes=(sample)=>{
+
+}
+
+const sample = [
+    'http://www.google.com/',
+    'http://www.spotify.com/us/',
+    'http://twitter.com/',
+    'http://google.com/nothing'
+]
+
+orderTimes(sample);
+
+const validURL=(sample)=>{
+    
+}   
+
+validURLs(sample);
