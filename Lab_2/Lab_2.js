22a3c5274c08730f8d7967a5573d34f075276943
Patrick Flinner <ptflinner@yahoo.com>
2018-02-26 19:49:40 -0800

    Finished Part 1 of Lab 2

---
+++ Lab_2/Lab_2.js
@@ -0,0 +1,56 @@
+const crypto =require('crypto')
+
+const hashes=crypto.getHashes()
+
+const hashingAlgorithm='sha256'
+
+const cryptoHash=(message)=>{
+
+    const hash=crypto.createHash(hashingAlgorithm)
+    
+    hash.update(message)
+    
+    const digest=hash.digest('hex')
+    
+    return digest
+}
+
+const leadingZero=(hashed)=>{
+    let count=0;
+    let hash=hashed.toString()
+    //console.log(hash);
+    for(let i=0;i<hash.length;i++){
+        if(hash[i]==='0'){
+            if(count<3){
+               count++ 
+            }
+            else{
+                return false;
+            }    
+        }
+        if(hash[i]!='0' && count==3){
+            return true;
+        }
+        else if(hash[i]!=0 && count!=3){
+            return false;
+        }
+    }
+    
+}
+
+const hashMessage=(message)=>{
+    let numLoops=0;
+    let loop=true;
+    let digest;
+    while(loop){
+        digest=cryptoHash((message+numLoops))
+        if(leadingZero(digest)){
+            loop=false;
+        }
+        numLoops++;
+    }
+    message+=numLoops;
+    console.log(`The '${hashingAlgorithm}' digest of '${message}' is: ${digest}`);
+}
+
+hashMessage('Hello, World!')
